00f3e1abe50f813747e7fd46f8eafe5403610030
Marc Strong <marcstrong@yahoo.com>
2020-03-29 21:27:31 -0500

    add link to LinkedIn button to take user to LinkedIn profile

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,11 +1,23 @@
 // GitHub button to GitHub profile
 var gitHubBtn = document.querySelector("#GitHub");
 
-// Generate quiz by selecting button to view the first question
+// Generate GitHub profile by selecting button to view 
 gitHubBtn.onclick = function() {
     gitHubPage();
 }
 
 function gitHubPage() {
     window.location.assign("https://github.com/StrongMarc")
+}
+
+// LinkedIn button to LinkedIn profile
+var linkedInBtn = document.querySelector("#linkedIn");
+
+// Generate LinkedIn profile by selecting button to view 
+linkedInBtn.onclick = function() {
+    linkedInPage();
+}
+
+function linkedInPage() {
+    window.location.assign("https://linkedin.com/in/marc-strong-3892548")
 }
